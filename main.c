e414e96b217bb517424e9d60e072aec27a067a75
ioliveirafarias <33236514+ioliveirafarias@users.noreply.github.com>
2024-01-26 20:58:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <Arduino.h>
 #define SERIAL_BAUDRATE 9600
 #define PROBE A0
-#define INPUT_VOLTAGE 5 
+#define FIXED_RESISTOR 2200
+//#define PROBE_NC A1
+#define INPUT_VOLTAGE 5
 #define MAX_ANALOG_READ 1023
-#define NC_ANALOG_READ_THRESHOLD 1022
+//#define NC_ANALOG_READ_THRESHOLD 0
 
 /**************************************************
  * Auxiliary functions
 **************************************************/
 
 float getVoltage( int analogReading ){
-  return (analogReading * INPUT_VOLTAGE) / (float) MAX_ANALOG_READ;
+  return (analogReading / (float) MAX_ANALOG_READ) * INPUT_VOLTAGE;
+}
+
+float getResistance( int analogReading ){
+  float aux = analogReading > (((float) MAX_ANALOG_READ)/2) ? 1-(analogReading / (float) MAX_ANALOG_READ) : 2*(analogReading / (float) MAX_ANALOG_READ);
+  return (aux) * FIXED_RESISTOR;
 }
 
 /**************************************************
@@ -19,17 +26,22 @@ float getVoltage( int analogReading ){
 
 void setup() {
   Serial.begin( SERIAL_BAUDRATE );
-  pinMode( PROBE, INPUT_PULLUP );
+  //pinMode( PROBE_NC, INPUT_PULLUP );
+  pinMode( PROBE, INPUT );
 }
 
 void loop() {
 
   int reading = analogRead(PROBE);
-  if ( reading >= NC_ANALOG_READ_THRESHOLD ) {
+  if ( reading == 0 ) {
     Serial.println( "NC / Nothing connected" );
   } else {
+    Serial.print("Analog read:");
+    Serial.println( reading );
     Serial.print("V:");
     Serial.println( getVoltage(reading) );
+    Serial.print("R:");
+    Serial.println( getResistance(reading) );    
   }  
   delay(2000);
 }
